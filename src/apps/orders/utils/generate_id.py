c82ec36eff5459da7e2de52969870602aaa98bff
Muhammadali1620 <muhammadvalievmuhammadali@gmail.com>
2025-03-07 19:49:17 +0500

    Add a function to generate a unique identifier for FoodOrder

--- src/apps/orders/utils/generate_id.py
+++ src/apps/orders/utils/generate_id.py
@@ -4,6 +4,17 @@ from django.apps import apps
 
 
 def new_id():
+    """
+    Generate a new unique ID for a FoodOrder.
+
+    This function generates a random 8-digit integer and checks if it already 
+    exists as a food_order_id in the FoodOrder model. If the generated ID 
+    already exists, the function recursively calls itself to generate a new ID 
+    until a unique one is found.
+
+    Returns:
+        str: A unique 8-digit ID as a string.
+    """
     FoodOrder = apps.get_model('orders', 'FoodOrder')  # Динамический импорт
     pk = random.randint(10000000, 99999999)
     if FoodOrder.objects.filter(food_order_id=pk).exists():
