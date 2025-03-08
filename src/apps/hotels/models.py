db6a47cf305acf4b617548f3abea313c0b6487de
HoKa <tolqinovoybek92@gmail.com>
2025-03-08 11:04:41 +0500

    i added Hotel model for using other programmer

--- src/apps/hotels/models.py
+++ src/apps/hotels/models.py
@@ -1,3 +1,26 @@
+from django.core.validators import RegexValidator, MinValueValidator, MaxValueValidator
 from django.db import models
 
-# Create your models here.
+from apps.base.models import AbstractBaseModel
+
+
+class Hotel(AbstractBaseModel):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, unique=True)
+    address = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255, unique=True)
+    phone_number = models.CharField(
+        max_length=255,
+        validators=[
+            RegexValidator(
+                regex=r"^\+966\d{10}$",
+                message="Example: +966 011 XXX XXXX",
+            )])
+    rating = models.DecimalField(
+        max_digits=3,
+        decimal_places=2,
+        validators=[MinValueValidator(0.00), MaxValueValidator(5.00)],
+    )
+
+    def __str__(self):
+        return self.name
