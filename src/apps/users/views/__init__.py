d83e27c397f49cb202315a9b8b76daa9527a1a45
Mukhsin0508 <mukhsinmukhtorov@arizona.edu>
2025-03-16 10:24:08 +0500

    feat(users): Update user profile and viewset functionality

--- src/apps/users/views/__init__.py
+++ src/apps/users/views/__init__.py
@@ -1,1 +1,2 @@
 from .viewset import *
+from .profile import *
--- src/apps/users/views/viewset.py
+++ src/apps/users/views/viewset.py
@@ -3,22 +3,38 @@ from django.contrib.auth import get_user_model
 
 from apps.base.views import CustomModelViewSet
 from apps.users.serializers import UserSerializer
-
+from apps.users.permissions import RoleBasedPermission
 
 
 class UserViewSet(CustomModelViewSet):
     queryset = get_user_model().objects.all()
     serializer_class = UserSerializer
+    department = 'users'  # This is used by RoleBasedPermission
 
     def get_permissions(self):
         """
-        Optionally override this method to customize permission checking,
-            or you can specify permission_classes in the class definition.
+        Use both IsAuthenticated and RoleBasedPermission for permission checking.
         :return:
         """
-        permission_classes = [IsAuthenticated]
+        permission_classes = [IsAuthenticated, RoleBasedPermission]
         return [permission() for permission in permission_classes]
 
     def get_object(self):
-        return self.request.user
+        """
+        If the action is 'me', return the current user.
+        Otherwise, use the default behavior.
+        """
+        if self.action == 'me':
+            return self.request.user
+        return super().get_object()
 
+    def get_queryset(self):
+        """
+        Filter the queryset based on the user's role.
+        Superusers and admins can see all users.
+        Other users can only see themselves.
+        """
+        user = self.request.user
+        if user.is_superuser or user.role == 'admin':
+            return self.queryset
+        return self.queryset.filter(id=user.id)
