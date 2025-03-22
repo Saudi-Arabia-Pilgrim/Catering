4b57fa28301c91c6343231c587e322e97bc9f805
HoKa <tolqinovoybek92@gmail.com>
2025-03-22 15:39:29 +0500

    i added guest serializer for HotelOrder

--- src/apps/guests/serializers/order_guests.py
+++ src/apps/guests/serializers/order_guests.py
@@ -5,20 +5,19 @@ from apps.guests.models import Guest
 from apps.hotels.models import Hotel
 
 
-class GuestSerializer(CustomModelSerializer):
+class GuestForHotelOrderSerializer(CustomModelSerializer):
     """
     Serializer for the Guest model.
 
     This serializer handles guest-related data, including retrieving guest details
     and creating new guest entries with an associated hotel.
     """
-    room_type = serializers.CharField(source="room.room_type", read_only=True)
+    room_type = serializers.CharField(source="room.room_type.name", read_only=True)
     total_price = serializers.SerializerMethodField()
 
     class Meta:
         model = Guest
         fields = [
-            "id",
             "full_name",
             "order_number",
             "room",
@@ -38,32 +37,5 @@ class GuestSerializer(CustomModelSerializer):
             return obj.room.gross_price * days_stayed
         return 0
 
-    def create(self, validated_data):
-        """
-        Create a new Guest instance.
-
-        Automatically assigns the hotel based on the `hotel_id` extracted from the view context.
-
-        Args:
-            validated_data (dict): Validated data for creating a Guest instance.
-
-        Returns:
-            Guest: Newly created Guest instance.
-        """
-        hotel_id = self.context["view"].kwargs.get("hotel_id")
-        if hotel_id:
-            validated_data["hotel"] = Hotel.objects.get(id=hotel_id)
-        return super().create(validated_data)
 
 
-
-
-# {
-#     "hotel": "Taiba Suites Madinah",
-#     "full_name": "Kimdirov Kimdir",
-#     "room_type": "2 Kishilik",
-#     "gender": "Male",
-#     "check_in": "12.03.2025",
-#     "check_out": "20.03.2025",
-#     "price": 120
-# }
