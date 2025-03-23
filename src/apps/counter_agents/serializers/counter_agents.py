02a42245a917b1a6036e5290d6411898dd26219c
Muhammadali1620 <muhammadvalievmuhammadali@gmail.com>
2025-03-23 15:34:20 +0500

    I added fields in contr agents serializer

--- src/apps/counter_agents/serializers/counter_agents.py
+++ src/apps/counter_agents/serializers/counter_agents.py
@@ -5,4 +5,5 @@ from apps.counter_agents.models import CounterAgent
 class CounterAgentSerializer(CustomModelSerializer):
     class Meta:
         model = CounterAgent
+        fields = '__all__'
         read_only_fields =["status"]
