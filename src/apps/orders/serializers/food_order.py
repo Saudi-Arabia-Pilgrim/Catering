af0ac2eb76583a7e585a543db77a3c66e37acad1
HoKa <tolqinovoybek92@gmail.com>
2025-03-29 16:36:18 +0500

    I added food orders, with views

--- src/apps/orders/serializers/food_order.py
+++ src/apps/orders/serializers/food_order.py
@@ -1,27 +1,56 @@
 from rest_framework import serializers
 
+from apps.base.exceptions import CustomExceptionError
 from apps.base.serializers import CustomModelSerializer
+from apps.counter_agents.models import CounterAgent
+from apps.menus.serializers import MenuSerializer
 from apps.orders.models import FoodOrder
 
 
-class FoodOrderSerializer(CustomModelSerializer):
+class CounterAgentSerializer(CustomModelSerializer):
+    class Meta:
+        model = CounterAgent
+        fields = [
+            "id",
+            "order_type",
+            "name",
+            "address",
+            "status",
+        ]
 
+
+class OnlyFoodOrderSerializer(CustomModelSerializer):
+    counter_agent = serializers.PrimaryKeyRelatedField(queryset=CounterAgent.objects.all())
     total_price = serializers.SerializerMethodField()
+    menu = MenuSerializer(many=True)
 
     class Meta:
         model = FoodOrder
         fields = [
             "food_order_id",
-            "hotel_order",
-            "expiration_date",
+            "counter_agent",
+            "menu",
+            "food",
             "order_type",
+            "order_time",
+            "expiration_date",
             "product_type",
-            "price",
             "address",
             "product_count",
+            "price",
             "total_price",
             "created_at"
         ]
+        read_only_fields = ["food_order_id", "total_price"]
+
+    def validate(self, attrs):
+        food = self.instance.food if self.instance else None
+        menu = self.instance.menu if self.instance else None
+        recipe = self.instance.recipe if self.instance else None
+
+        if bool(attrs.get("food", food)) + bool(attrs.get("menu", menu)) + bool(attrs.get("recipe", recipe)) != 1:
+            raise CustomExceptionError(code=400, detail="Only one of 'food', 'menu', or 'recipe' must be set.")
+        return attrs
 
     def get_total_price(self, obj):
         return obj.total_price
