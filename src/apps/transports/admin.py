b93f2d9bdc6cfcf25952b9a10a66b80b383cdeec
Mukhsin Mukhtorov <muxsinmuxtorov01@gmail.com>
2025-04-25 20:28:30 +0500

    Add admin interfaces for Transport and Order models
    This commit introduces `TransportAdmin` and `OrderAdmin` classes for managing the Transport and Order models in the Django admin panel. It includes list displays, filters, search fields, readonly fields, and organized fieldsets to enhance usability and data visibility.

--- src/apps/transports/admin.py
+++ src/apps/transports/admin.py
@@ -1,3 +1,44 @@
 from django.contrib import admin
 
-# Register your models here.
+from apps.transports.models import Transport
+from apps.transports.models.orders import Order
+
+
+@admin.register(Transport)
+class TransportAdmin(admin.ModelAdmin):
+    """Admin interface for a Transport model."""
+    list_display = ('id', 'name', 'name_of_driver', 'phone_number', 'amount_of_people', 'status', 'created_at')
+    list_filter = ('status',)
+    search_fields = ('name', 'name_of_driver', 'phone_number', 'address')
+    prepopulated_fields = {'slug': ('name',)}
+    readonly_fields = ('created_at', 'updated_at', 'created_by', 'updated_by')
+    fieldsets = (
+        (None, {
+            'fields': ('name', 'slug', 'name_of_driver', 'address', 'phone_number', 'amount_of_people', 'status')
+        }),
+        ('Timestamps and Audit', {
+            'fields': ('created_at', 'created_by', 'updated_at', 'updated_by'),
+            'classes': ('collapse',)
+        }),
+    )
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    """Admin interface for an Order model."""
+    list_display = ('order_number', 'transport', 'perform_date', 'from_location', 'to_location', 'status', 'service_fee')
+    list_filter = ('status', 'perform_date')
+    search_fields = ('order_number', 'from_location', 'to_location')
+    readonly_fields = ('id', 'created_at', 'updated_at', 'created_by', 'updated_by', 'order_number')
+    fieldsets = (
+        (None, {
+            'fields': ('order_number', 'transport', 'perform_date', 'from_location', 'to_location', 'status')
+        }),
+        ('Additional Information', {
+            'fields': ('passenger_count', 'service_fee')
+        }),
+        ('Timestamps and Audit', {
+            'fields': ('created_at', 'created_by', 'updated_at', 'updated_by'),
+            'classes': ('collapse',)
+        }),
+    )
