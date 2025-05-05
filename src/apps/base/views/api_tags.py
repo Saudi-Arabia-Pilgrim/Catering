43f1731a02aa0ff3ba91540fe634f34535202a45
Mukhsin Mukhtorov <muxsinmuxtorov01@gmail.com>
2025-05-05 16:27:00 +0500

    Refactor API base classes and update user-related views
    Replaced `APIView` with `BaseAPIView` in user views for consistency and code reuse. Introduced `AutoTaggedAPIViewSets` to enhance tagging functionality in viewsets. Updated URLs by changing the root path for user-related routers.

--- src/apps/base/views/api_tags.py
+++ src/apps/base/views/api_tags.py
@@ -12,3 +12,21 @@ class AutoTaggedAPIView:
         }
 
         extend_schema_view(**method_decorators)(cls)
+
+class AutoTaggedAPIViewSets:
+    def __init_subclass__(cls, **kwargs):
+        super().__init_subclass__(**kwargs)
+
+        tag = cls.__module__.split('.')[1].capitalize()
+
+        viewset_methods = [
+            'list', 'create', 'retrieve', 'update', 'partial_update', 'destroy'
+        ]
+
+        method_decorators = {
+            method: extend_schema(tags=[tag])
+            for method in viewset_methods
+            if hasattr(cls, method)
+        }
+
+        extend_schema_view(**method_decorators)(cls)
