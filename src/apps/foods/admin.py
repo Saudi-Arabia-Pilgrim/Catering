5480ac2dd588c04a5643c69db3e9336572e1e2fe
HoKa <tolqinovoybek92@gmail.com>
2025-06-01 20:32:49 +0500

    Muhammadali add to admin something

--- src/apps/foods/admin.py
+++ src/apps/foods/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from apps.foods.models import Food, RecipeFood
+from apps.foods.models import Food, RecipeFood, FoodSection
 
 admin.site.register(Food)
 admin.site.register(RecipeFood)
+admin.site.register(FoodSection)
