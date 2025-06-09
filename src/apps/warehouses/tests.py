500d90192d5c8a5cd3baa9c8255e4237c6c7aefc
Muhammadali1620 <muhammadvalievmuhammadali@gmail.com>
2025-06-09 14:37:44 +0500

    Debug counter_agent 6

--- src/apps/warehouses/tests.py
+++ src/apps/warehouses/tests.py
@@ -1,6 +1,21 @@
-# lists = ["olma", "nok", "uzum", "shoftoli"]
-# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-#
-# lists.sort(reverse=False)
-#
-# print(lists)
+import requests
+
+product_ids = []
+
+products_url = "https://catering.mukhsin.space/api/v1/products/"
+warehouses_url = "https://catering.mukhsin.space/api/v1/warehouses/"
+
+next_exists = True
+
+while next_exists:
+    products = requests.get(url=products_url).json()
+    if products["next"] == None:
+        next_exists = False
+    products_url = products["next"]
+
+    for i in products["results"]:
+        product_ids.append(i["id"])
+
+for product_id in product_ids:
+    response = requests.post(url=warehouses_url, data={"arrived_amount": 300, "gross_price": 300, "product": product_id})
+    print(response.status_code)
