ce031202aa3361e136090b159ba327611fc287ac
HoKa <tolqinovoybek92@gmail.com>
2025-06-30 21:10:14 +0500

    i changed from int to uuid

--- src/apps/expenses/urls.py
+++ src/apps/expenses/urls.py
@@ -10,8 +10,8 @@ from apps.expenses.views import (
 
 urlpatterns = [
     path("monthly-salary/list/", MonthlySalaryListAPIView.as_view(), name="list"),
-    path("monthly-salary/<int:id>/", MonthlySalaryGenericAPIView.as_view(), name="monthly_salary"),
-    path("monthly-salary/update/<int:id>/", MonthlySalaryUpdateAPIView.as_view(), name="monthly_salary_update"),
+    path("monthly-salary/<uuid:id>/", MonthlySalaryGenericAPIView.as_view(), name="monthly_salary"),
+    path("monthly-salary/update/<uuid:id>/", MonthlySalaryUpdateAPIView.as_view(), name="monthly_salary_update"),
     path('hiring-expenses/list/', HiringExpenseListAPIView.as_view(), name='list'),
     path("hiring-expense/", HiringExpenseCreateAPIView.as_view(), name="hiring_expense_create"),
     path("hiring-expense/<uuid:pk>/", HiringExpenseRetrieveUpdateDestroyAPIView.as_view(), name="hiring_expense_detail"),
