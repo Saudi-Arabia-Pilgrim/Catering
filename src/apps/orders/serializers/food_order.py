58707490ecf5adb1adb6cf843d26d8ccf3d958b2
Muhammadali1620 <muhammadvalievmuhammadali@gmail.com>
2025-07-13 10:54:55 +0500

    Change process ready orders

--- src/apps/orders/serializers/food_order.py
+++ src/apps/orders/serializers/food_order.py
@@ -1,10 +1,8 @@
 from rest_framework import serializers
 
+from apps.base.exceptions import CustomExceptionError
 from apps.base.serializers import CustomModelSerializer
-from apps.counter_agents.serializers import CounterAgentSerializer
 from apps.orders.models import FoodOrder
-from apps.foods.serializers import FoodSerializerForFoodOrder
-from apps.menus.serializers import MenuSerializer, RecipeSerializer
 
 
 class OnlyFoodOrderSerializer(CustomModelSerializer):
@@ -60,6 +58,7 @@ class OnlyFoodOrderSerializer(CustomModelSerializer):
 
     def to_representation(self, instance):
         data = super().to_representation(instance)
+        data["status"] = instance.get_status_display()
         products = ["food", "menu", "recipe"]
         for product in products:
             obj = getattr(instance, product, None)
@@ -69,11 +68,83 @@ class OnlyFoodOrderSerializer(CustomModelSerializer):
         return data
 
 
-class FoodOrderRetrieveSerializer(OnlyFoodOrderSerializer):
-    food = FoodSerializerForFoodOrder(read_only=True)
-    menu = MenuSerializer(read_only=True)
-    recipe = RecipeSerializer(read_only=True)
-    counter_agent = CounterAgentSerializer(read_only=True)
+class FoodOrderRetrieveSerializer(CustomModelSerializer):
+    total_price = serializers.SerializerMethodField(read_only=True)
+    product_type = serializers.SerializerMethodField(read_only=True)
+    order_type = serializers.SerializerMethodField(read_only=True)
+    experience_date = serializers.SerializerMethodField(read_only=True)
+    counter_agent_name = serializers.CharField(source="counter_agent.name", read_only=True)
 
-    class Meta(OnlyFoodOrderSerializer.Meta):
-        pass
+    class Meta:
+        model = FoodOrder
+        fields = [
+            "id",
+            "food_order_id",
+            "food",
+            "menu",
+            "recipe",
+            "product_type",
+            "product_count",
+            "order_type",
+            "order_time",
+            "experience_date",
+            "counter_agent",
+            "counter_agent_name",
+            "address",
+            "price",
+            "status",
+            "total_price",
+            "created_at",
+        ]
+
+        read_only_fields = [
+            "id",
+            "food_order_id",
+            "food",
+            "menu",
+            "recipe",
+            "product_type",
+            "product_count",
+            "order_type",
+            "order_time",
+            "experience_date",
+            "counter_agent",
+            "counter_agent_name",
+            "address",
+            "price",
+            "total_price",
+            "created_at",
+        ]
+
+    def get_total_price(self, obj):
+        return obj.total_price
+
+    def get_order_type(self, obj):
+        return obj.get_product_type_display()
+
+    def get_product_type(self, obj):
+        return obj.get_order_type_display()
+    
+    def get_experience_date(self, obj):
+        return obj.experience_date_str
+
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        data["status"] = instance.get_status_display()
+        products = ["food", "menu", "recipe"]
+        for product in products:
+            obj = getattr(instance, product, None)
+            if obj:
+                data[f"{product}_name"] = obj.name
+                break
+        return data
+
+    def update(self, instance, validated_data):
+        status = validated_data.get("status")
+        if status is None:
+            raise CustomExceptionError(detail="Status most be required", code="400")
+        if status == FoodOrder.Status.PENDING:
+            raise CustomExceptionError(detail="Status is already pending. Place select other statuses.", code="400")
+        if instance.status != FoodOrder.Status.PENDING:
+            raise CustomExceptionError(detail="Status is already updated", code="400")
+        return super().update(instance, validated_data)
